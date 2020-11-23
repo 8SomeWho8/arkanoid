e6a7e5b15e549ee533d2e02b9617bd01f8a1794e
8SomeWho8 <rozhnik22@gmail.com>
2020-11-23 11:03:54 +0300

    Исправил ошибки в коде

--- classes.py
+++ classes.py
@@ -6,6 +6,7 @@ class Platform():
         #Платформа создается снизу в центре экрана в виде маленького черного прямоугольника
         self.x0 = 300
         self.y0 = 550
+        self.v = 5
         self.width = 60
         self.height = 15
         rect(screen, (0,0,0), (self.x0-self.width/2, self.y0-self.height/2, self.width, self.height))
@@ -14,10 +15,11 @@ class Platform():
     def move(self, dir):
         if dir == "left" and self.x0 >= 30:
             self.x0 -= self.v
-        if dir == "right" and self.x0 <=570:
+        if dir == "right" and self.x0 <= 570:
             self.x0 += self.v
 
     pass
+
 
 
 class Balls():
@@ -29,12 +31,11 @@ class Balls():
         self.vx=randint(-5, 5)
         self.vy=randint(1, 5)
 
-    def move(self):
+    def move(self, platform):
         if self.x0 <= 0 or self.x0 >=600:
             self.x0 = 599
             self.vx = -self.vx
-        if (self.x0 >= Platform.x0 - Platform.width/2 \
-                and self.x0 <= Platform.x0 + Platform.width/2 \
+        if (platform.x0 - platform.width/2 <= self.x0 <= platform.x0 + platform.width/2
                 and self.y0 <= Platform.y0):
             self.vy = -self.vy
         self.x0 +=self.vx
@@ -42,20 +43,22 @@ class Balls():
 
         pass
 
+
 class Targets():
     def __init__(self):
         self.height = 15
         self.width = 30
         pass
 
-    def create_bricks():
-        a = [48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552
+    def create_bricks(self):
+        a = [48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
+             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
+             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
+             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
              48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552]
         for i in range(0, 75):
             rect(screen, (0,0,0), a[i]-self.width/2, 20+(i//15)*20-self.height/2, self.width, self.height)
+
 
 class Bonuses():
     def __init__(self):
