9d82fc350098b78a1f11f436d65694ca5c5c2eea
DaniilSadykov <71903089+DaniilSadykov@users.noreply.github.com>
2020-11-23 13:34:41 +0300

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,20 +1,6 @@
 #Б02-010 Артаева Рожков Садыков
 from random import randint
 import numpy as np
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-MAGENTA = (255, 0, 255)
-CYAN = (0, 255, 255)
-BLACK = (0, 0, 0)
-LILAC = (182, 102, 210)
-AMBER = (255, 191, 0)
-VINOUS = (113, 25, 25)
-RED_CORAL = (255, 127, 80)
-BLUE_STEEL = (80, 127, 255)
-LIME = (204, 255, 0)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, LILAC, AMBER, VINOUS, RED_CORAL, BLUE_STEEL, LIME]
 #Имеется игровое поле 600 на 600
 class Platform():
     def __init__(self):
@@ -24,7 +10,9 @@ class Platform():
         self.v = 5
         self.width = 60
         self.height = 15
-        rect(screen, (0,0,0), (self.x0-self.width/2, self.y0-self.height/2, self.width, self.height))
+        
+        rect(screen, (0,0,0), (self.x0-self.width/2, self.y0-self.height/2, self.width, self.height)) #рисование картинки платформы
+        platform = pygame.Rect(self.x0-self.width/2, self.y0-self.height/2, self.width, self.height) #физичная платформа
         self.lives = 3
 
     def move(self, dir):
@@ -48,6 +36,12 @@ class Ball():
         self.v = 50
         self.vx = 0
         self.vy = 50
+        self.radius = 10 #радиус шарика
+        circle(screen, (0,0,0), (self.x, self.y), self.radius) #рисую круг
+       
+        self.innersquareradius = 7  #Внутри круга сделал квадрат поменьше, innercircleradius это длина половины стороны квадратика
+        
+        innersquare = pygame.Rect(self.x-self.innersquareradius, self.y-self.innersquareradius, 2*self.innersquareradius, 2*self.innersquareradius)  #Создал физичный квадратик
 
     def move(self, platform):
         x = platform.x0
@@ -57,13 +51,15 @@ class Ball():
         if self.x <= 0 or self.x >=600:
             self.x = 599
             self.vx = -self.vx
-        if (x - w/2 <= self.x <= x + w/2
-                and self.y <= y + h//2):
-            alpha = int(self.v * np.atan(w/(2 * x) * np.tan(np.pi/9)))
-            self.vx = self.v * np.cos(alpha)
-            self.vy = self.v * np.sin(alpha)
+        if innersquare.colliderect(Platform): """ нужныйобьект.colliderect(обьект с которым проверяется столкновение) - проверяет наслоение двух прямоугольников) """
+            self.vy *= -1
+        #if (x - w/2 <= self.x <= x + w/2
+        #        and self.y <= y + h//2):
+        #    alpha = int(self.v * np.atan(w/(2 * x) * np.tan(np.pi/9)))
+        #   self.vx = self.v * np.cos(alpha)
+        #    self.vy = self.v * np.sin(alpha)
         self.x += self.vx
-        self.vy += self.vy
+        self.y += self.vy
 
         pass
 
@@ -75,15 +71,8 @@ class Targets():
         pass
 
     def create_bricks(self):
-        a = [48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552,
-             48, 84, 120, 156, 192,	228, 264, 300, 336, 372, 408, 444, 480, 516, 552]
-        for i in range(0, 75):
-            rect(screen, (0,0,0), a[i]-self.width/2, 20+(i//15)*20-self.height/2, self.width, self.height)
-
-
+        brick_list = [pygame.Rect(6 + 36 * i, 20 + 20 * j, 30, 15) for i in range(15) for j in range(5)] #создание физичных прямоугольников
+        color_list = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(15) for j in range(5)] #случайный цвет
 class Bonuses():
     def __init__(self):
         pass
