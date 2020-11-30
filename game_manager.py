858910115cbb372168ccbf7b23fcda77a6fb51d9
DaniilSadykov <71903089+DaniilSadykov@users.noreply.github.com>
2020-11-30 14:21:53 +0300

    Добавил почти рабочую коллизию с кирпичами

--- game_manager.py
+++ game_manager.py
@@ -21,21 +21,24 @@ class GameManager:
         ball_1 = Ball()
         balls = [ball_1]
         targets = Targets()
-
+        
         while not game_over:
             clock.tick(FPS)
             screen.fill(WHITE)
             for ball in balls:
                 ball.move(platform)
+            
+            # метод collidelist() находит индекс кирпича с которым столкнулся мяч, или -1 если столкновения не было
+            hit_index = ball_1.inner_square.collidelist(targets.brick_list) # hit_index=главный_обьект.collidelist(обьект, с которым проверяется столкновение)
+            if hit_index != -1:
+                hit_rect = targets.brick_list.pop(hit_index) # находим по индексу нужный кирпич и одновременно удаляем его из списка
+                detect_collision(ball_1, hit_rect) # функция для отражения мяча от кирпича
+                hit_color = targets.color_list.pop(hit_index) # аналогично с цветом кирпича
+                detect_collision(ball_1, hit_rect) # функция для отражения мяча от кирпича
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     game_over = True
                     pygame.quit()
-                #elif event.type == pygame.KEYDOWN:
-                 #   if event.key == pygame.K_LEFT:
-                    #    platform.move("left")
-                  #  elif event.key == pygame.K_RIGHT:
-                     #   platform.move("right")
             key = pygame.key.get_pressed()
             if key[pygame.K_LEFT]:
                 platform.move("left")
