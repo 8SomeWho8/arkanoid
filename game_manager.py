28349172e3ed53cadb4886a8c3ab0bea74dc051a
DaniilSadykov <71903089+DaniilSadykov@users.noreply.github.com>
2020-12-07 13:28:10 +0300

    Восстановил старую верисю

--- game_manager.py
+++ game_manager.py
@@ -2,27 +2,21 @@ import pygame
 from pygame.draw import *
 from platform_ball_targets import *
 pygame.init()
-
 FPS = 60
 screen = pygame.display.set_mode((800, 800))
 WHITE = (255, 255, 255)
-
-
 class GameManager:
     def __init__(self):
         global screen
-
     def main_loop(self, screen):
         clock = pygame.time.Clock()
         game_over = False
-
         platform = Platform()
         platform.draw(screen)
         ball_1 = Ball()
         balls = [ball_1]
         targets = Targets()
         k = 0
-        targets.gift_bricks ()
         surf = pygame.image.load("fon.jpg")
         surf = pygame.transform.scale(surf, [800, 800])
         
@@ -30,15 +24,10 @@ class GameManager:
             clock.tick(FPS)
             screen.fill(WHITE)
             screen.blit(surf, (0, 0))
-            
+
             # метод collidelist() находит индекс кирпича с которым столкнулся мяч, или -1 если столкновения не было
             hit_index = ball_1.inner_square.collidelist(targets.brick_list) # hit_index=главный_обьект.collidelist(обьект, с которым проверяется столкновение)
             if hit_index != -1:
-                
-                for i in range (len(targets.gifted_bricks_list)): #поиск мертвого кирпича в списке одаренных
-                if hit_index == targets.gifted_bricks_list[i]:
-                    trigger_bonus (ball_1.x, ball_1.y) #запускается функция появления и дальнейшей жизни бонуса, а также передается примерное место смерти кирпича (не придумал как запросить координаты мертвого кирпича, решил взять координату шарика, она не сильно отличается) 
-                
                 k += 1
                 hit_rect = targets.brick_list.pop(hit_index) # находим по индексу нужный кирпич и одновременно удаляем его из списка
                 detect_collision(ball_1, hit_rect) # функция для отражения мяча от кирпича
@@ -73,7 +62,5 @@ class GameManager:
             score = pygame.font.SysFont('arial', 20).render('Score:'+str(k), True, RED_CORAL)
             screen.blit(score, (700, 700))
             pygame.display.update()
-
-
 gm = GameManager()
 gm.main_loop(screen)
