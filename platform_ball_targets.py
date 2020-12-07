1f2776d06a51b7ed26858436bde066b7a1e1eb8d
DaniilSadykov <71903089+DaniilSadykov@users.noreply.github.com>
2020-12-07 13:31:03 +0300

    восстановил старую версию

--- platform_ball_targets.py
+++ platform_ball_targets.py
@@ -3,7 +3,6 @@ from random import *
 import numpy as np
 import pygame
 from pygame.draw import *
-
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -18,8 +17,6 @@ RED_CORAL = (255, 127, 80)
 BLUE_STEEL = (80, 127, 255)
 LIME = (204, 255, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, LILAC, AMBER, VINOUS, RED_CORAL, BLUE_STEEL, LIME]
-
-
 class Platform:
     def __init__(self):
         # Платформа создается снизу в центре экрана в виде маленького черного прямоугольника
@@ -31,40 +28,33 @@ class Platform:
         # физичная платформа
         self.physical_obj = pygame.Rect(self.x - self.width // 2, self.y - self.height // 2, self.width, self.height)
         self.lives = 3
-
     def move(self, direction: str):
         if direction == "left" and self.x > 40:
             self.x -= self.v
         if direction == "right" and self.x < 760:
             self.x += self.v
         self.physical_obj = pygame.Rect(self.x - self.width // 2, self.y - self.height // 2, self.width, self.height)
-
     def draw(self, screen):
         rect(screen, BLACK, self.physical_obj)
-
-
 class Ball:
     def __init__(self):
         # Шары создаются в нижней половине экрана автоматически со временем (повышающаяся сложность)
         # или после смерти старого шара (шар умирает = вылетает снизу экрана (игрок не смог его отбить))
         # Координата появления случайная, скорость направлена вверх,
         # но в самое первое перемещение будет заменена
-        self.x = -20
-        self.y = -20
-        self.v = 2.5
+        self.x = 530
+        self.y = 666
+        self.v = 10
         self.vx = 0
-        self.vy = -2.5
+        self.vy = -10
         self.radius = 9  # радиус шарика
 
         self.on_platform = True
-
         # Внутри круга сделал квадрат поменьше, inner_square_radius - это длина половины стороны квадратика
         self.inner_square_radius = self.radius / 2**0.5
-
         # Создал физичный квадратик
         self.inner_square = pygame.Rect(self.x - self.inner_square_radius, self.y - self.inner_square_radius,
                                         2 * self.inner_square_radius, 2 * self.inner_square_radius)
-
     def move_freely(self, platform):
         x = platform.x
         w = platform.width
@@ -82,17 +72,13 @@ class Ball:
         self.y += self.vy
         self.inner_square = pygame.Rect(self.x - self.inner_square_radius, self.y - self.inner_square_radius,
                                         2 * self.inner_square_radius, 2 * self.inner_square_radius)
-
     def move_on_platform(self, platform, position_on_platform: float):
         self.x = platform.x + position_on_platform*platform.width
         self.y = platform.y - platform.height/2 - self.radius
         self.inner_square = pygame.Rect(self.x - self.inner_square_radius, self.y - self.inner_square_radius,
                                         2 * self.inner_square_radius, 2 * self.inner_square_radius)
-
     def draw(self, screen):
         circle(screen, BLACK, (round(self.x), round(self.y)), self.radius)
-
-
 # Функция детальной проверки коллизии и отражения мяча
 def detect_collision(obj1, obj2):
     dx, dy = obj1.vx/obj1.v, obj1.vy/obj1.v  # функция находит единичный вектор скорости мяча
@@ -106,50 +92,33 @@ def detect_collision(obj1, obj2):
         delta_y = obj1.inner_square.bottom - obj2.top
     else:
         delta_y = obj2.bottom - obj1.inner_square.top
-
     # Отражение скоростей
     if delta_x > delta_y:
         obj1.vy *= -1
     elif delta_y > delta_x:
         obj1.vx *= -1
-
-
 class Targets:
     def __init__(self):
-    
-        self.height = 15
-        self.width = 30
+        self.height = 20
+        self.width = 40
         self.horizontal_number = 16
         self.vertical_number = 5
-        self.brick_list = [pygame.Rect(6 + 36 * i, 20 + 20 * j, self.width, self.height) for i in range(self.horizontal_number)
+        self.brick_list = [pygame.Rect(8 + 48 * i, 27 + 27 * j, self.width, self.height) for i in range(self.horizontal_number)
                            for j in range(self.vertical_number)]  # создание физичных прямоугольников
         self.color_list = [choice(COLORS) for i in range(self.horizontal_number * self.vertical_number)]  # случайный цвет
-
     def draw_bricks(self, screen):
+        surf1 = pygame.image.load("image003.png")
+        surf1 = pygame.transform.scale(surf1, (self.width, self.height))
         for i in range(len(self.brick_list)):
             rect(screen, self.color_list[i], self.brick_list[i])
-
+            screen.blit(surf1, self.brick_list[i])
     def move(self):
         self.vertical_number += 1
         for i in range(self.horizontal_number):
             for i in range(len(self.brick_list)):
                 self.brick_list[i].move_ip(0, 20)
-            self.brick_list.insert(0, pygame.Rect(6 + 36 * i, 20 + 20 * (self.vertical_number - 1),
+            self.brick_list.insert(0, pygame.Rect(8 + 48 * i, 27 + 27 * (self.vertical_number - 1),
                                                   self.width, self.height))
             self.color_list.insert(0, choice(COLORS))
-    
-    def gift_bricks(self):
-        self.gifted_bricks_list = []
-        for i in range (len(self.brick_list)):
-            a = randint(1, 10)
-            if a == 10:
-                self.gifted_bricks_list.append(i)
-                
-                
-            
-def trigger_bonus (x, y):
-    pass
-        
-
 if __name__ == "__main__":
     print("This module is not for direct call!")
